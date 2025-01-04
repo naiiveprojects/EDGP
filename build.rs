ce9eb189b57769796bfc1e5b64aeb19820d16378
illlustr <still.lkmn@gmail.com>
2025-01-04 22:01:39 +0700

    init main

--- build.rs
+++ build.rs
@@ -3,17 +3,14 @@ use std::env;
 
 fn main() {
     println!("cargo:rerun-if-changed=res/resources.rc");
-
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR").expect("E:env not set");
     let status = Command::new("windres")
         .args(&["res/resources.rc", "-O", "coff", "-o"])
-        .arg(&format!("{}/resources.res", out_dir))
+        .arg(format!("{}/resources.res", out_dir))
         .status()
-        .expect("Failed to run windres");
-
+        .expect("E:run windres");
     if !status.success() {
         panic!("windres failed");
     }
-
     println!("cargo:rustc-link-arg-bins={}/resources.res", out_dir);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,59 @@
 
 use std::process::{Command, exit};
 use std::env;
-use std::path::Path;
-
-fn exists(p: &Path) {
-    if !p.exists() { exit(1); }
-}
+use std::path::PathBuf;
 
 fn main() {
-    let slf = env::current_exe().expect("E:path");
-    let dir = slf.parent().expect("E:directory");
-    let n = slf.file_stem().expect("E:name").to_string_lossy();
-    let exe = dir.join(format!("{}/Bin/win.exe", n));
-    let pck = dir.join(format!("{}/Data/main.pck", n));
-    exists(&exe);
-    exists(&pck);
-    Command::new(exe)
-        .arg("--main-pack")
-        .arg(pck)
-        .spawn().expect("E:process");
+    let exe = env::current_exe().expect("path"); // Executable path
+    let bdr = exe.parent().expect("directory").to_path_buf(); // Base directory
+    let fnm = exe.file_name().expect("filename").to_string_lossy(); // File name
+    let bnm = fnm.split_once('.').map(|(base, _)| base).unwrap_or(&fnm); // Base name
+    let fdr = bdr.join(bnm); // Folder directory
+    let dbg = fnm.chars().filter(|&c| c == '.').count() > 1; // (. before .exe)
+    
+    
+    if fdr.exists() {
+        run(&fdr, dbg);
+    } else if dbg {
+        alt(&bdr, &bnm, &exe);
+    } else {
+        exit(1);
+    }
+}
+
+fn run(fdr: &PathBuf, dbg: bool) {
+    let exe = fdr.join("Bin/win.exe");
+    let pck = fdr.join("Data/main.pck");
+
+    if exe.exists() && pck.exists() {
+        if dbg {
+            Command::new("cmd")
+                .arg("/C")
+                .arg(&exe)
+                .arg("--main-pack")
+                .arg(&pck)
+                .arg("-v")
+                .status()
+                .expect("run");
+        } else {
+            Command::new(&exe)
+                .arg("--main-pack")
+                .arg(&pck)
+                .spawn()
+                .expect("run");
+        }
+    }
+}
+
+fn alt(bdr: &PathBuf, bnm: &str, exe: &PathBuf) {
+    let alt = bdr.join(format!("{}.exe", bnm));
+
+    if alt.exists() && alt != *exe {
+        Command::new("cmd")
+            .arg("/C")
+            .arg(&alt)
+            .args(&["-v", "-d"])
+            .status()
+            .expect("alt");
+    }
 }
