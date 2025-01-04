a5637a4e0d309861b24a5ee220cc726f3558ccb9
illlustr <still.lkmn@gmail.com>
2025-01-04 15:12:40 +0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
-use std::{env, process::{Command, exit}};
+use std::{env, process::{Command, self}};
 
 fn main() {
-    let slf = env::current_exe().unwrap_or_else(|_| exit(1));
-    let fnm = slf.file_name().unwrap_or_else(|| exit(1)).to_string_lossy();
-    let trg = fnm.split_once('.')
-        .map(|(name, _)| format!("{}.exe", name))
-        .unwrap_or_else(|| exit(1));
+    let slf = env::current_exe().unwrap_or_else(|_| process::exit(1));
+    let fnm = slf.file_name().unwrap_or_else(|| process::exit(1)).to_string_lossy();
+    let bin_name = fnm.split_once('.')
+        .map(|(name, _)| name)
+        .unwrap_or_else(|| process::exit(1));
 
-    let pth = slf.parent().unwrap_or_else(|| exit(1)).join(&trg);
-    if !pth.exists() || pth == slf {
-        exit(1);
+    let bin_dir = slf.parent().unwrap_or_else(|| process::exit(1)).join(bin_name);
+    if !bin_dir.exists() || !bin_dir.is_dir() {
+        process::exit(1);
     }
-    let sts = Command::new(pth)
-        .args(&vec!["-v", "-d"])
+
+    let engine_path = bin_dir.join("Bin").join("Engine.exe");
+    if !engine_path.exists() {
+        process::exit(1);
+    }
+
+    let data_path = bin_dir.join("Data").join("Master.pck");
+    if !data_path.exists() {
+        process::exit(1);
+    }
+
+    let sts = Command::new(engine_path)
+        .args(&["--main-pack", data_path.to_str().unwrap(), "-v"])
         .status()
-        .unwrap_or_else(|_| exit(1));
-    exit(sts.code().unwrap_or(1));
+        .unwrap_or_else(|_| process::exit(1));
+    process::exit(sts.code().unwrap_or(1));
 }
